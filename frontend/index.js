a87bf96b7ff3d8d8e5fe0bebbc71466177b9ea3f
richrosenthal <rrosenthal2013@gmail.com>
2020-10-20 22:06:57 -0400

    flesh out fetchEquipment function

--- frontend/index.js
+++ frontend/index.js
@@ -1,15 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-
+  fetchEquipment();
 })
 
 
-
+const BASE_URL = "http://localhost:3000/"
 //read - fetch equipment
 
 function fetchEquipment(){
-  fetch()
-  .then
-  .then
+  console.log("here")
+  fetch(`${BASE_URL}/equipment`)
+  .then(resp =>resp.json())
+  .then(equipments => {
+
+    for (const equipment of equipments){
+      console.log("rails obj", equipment)
+      let e = new Equipment(equipment.id, equipment.name,
+        equipment.description, equipment.location, equipment.department)
+
+      console.log(e)
+    }
+  })
 }
 
 // create a new piece of equipment
