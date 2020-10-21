c395ebc394d3229aa7de5cb0e2c3c8950e5cf458
richrosenthal <rrosenthal2013@gmail.com>
2020-10-20 20:26:47 -0400

    update

--- backend/PM_API/db/seeds.rb
+++ backend/PM_API/db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Equipment.create(name: "Mitutoyo Caliper", description: "12\" Caliper", location: "Receiving Inspection", department: "QA")
