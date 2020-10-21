547f9a0217d354dbaaab9feed7c4f19bbe038857
richrosenthal <rrosenthal2013@gmail.com>
2020-10-20 21:18:59 -0400

    create constructor for object

---
+++ frontend/src/task.js
@@ -0,0 +1,7 @@
+class Task{
+  constructor(description, completion, tools_needed){
+    this.description = description;
+    this.completion = completion;
+    this.tools_needed = tools_needed;
+  }
+}
