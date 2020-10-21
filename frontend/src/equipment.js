5879a4a23d9b829e7d7511eb808fa259a2c894ec
richrosenthal <rrosenthal2013@gmail.com>
2020-10-20 21:21:16 -0400

    create constructor for equipment object

---
+++ frontend/src/equipment.js
@@ -0,0 +1,8 @@
+class Equipment{
+  constructor(name, description, location, department){
+    this.name = name;
+    this.description = description;
+    this.location = location;
+    this.department = department;
+  }
+}
