68628555279d2a6a1e136c0134f73cf2eb439b34
richrosenthal <rrosenthal2013@gmail.com>
2020-11-01 10:46:13 -0500

    create additional seeds

--- backend/PM_API/db/seeds.rb
+++ backend/PM_API/db/seeds.rb
@@ -6,3 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Equipment.create(name: "Mitutoyo Caliper", description: "12\" Caliper", location: "Receiving Inspection", department: "QA")
+Equipment.create(name: "Mitutoyo Caliper", description: "6 Caliper", location: "Receiving Inspection", department: "QA")
+Task.create(description: "Grease", completion: false, tools_needs: "WD40", equipment_id: 1)
+Task.create(description: "Grease", completion: false, tools_needs: "WD40", equipment_id: 2)
+Task.create(description: "Tighten Clamp", completion: true, tools_needs: "Screwdriver", equipment_id: 1)
