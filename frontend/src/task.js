25a27d5d198daf903ce5a3f23e57ddfc3f201613
richrosenthal <rrosenthal2013@gmail.com>
2020-11-01 11:06:03 -0500

    fixes format

--- frontend/src/task.js
+++ frontend/src/task.js
@@ -1,7 +1,41 @@
 class Task{
-  constructor(description, completion, tools_needed){
+  constructor(description, completion, tools_needs, equipment_id){
     this.description = description;
     this.completion = completion;
-    this.tools_needed = tools_needed;
+    this.tools_needs = tools_needs;
+    this.equipment_id = equipment_id;
   }
+
+  renderTask() {
+    let taskDiv = document.getElementById("task-container")
+
+    taskDiv.innerHTML +=
+    `
+    <tr>
+      <th>${this.id}</th>
+
+      <th>
+
+        ${this.description}
+      </th>
+      <th>
+
+        ${this.completion}
+      </th>
+      <th>
+
+        ${this.tools_needs}
+      </th>
+      <th>
+
+        ${this.equipment_id}
+      </th>
+    </tr>
+    <button class="delete-button" data-id=${this.id} onclick="deleteEquipment()">Delete EQ</button>
+    <br>
+    `
+
+
+  }
+
 }
