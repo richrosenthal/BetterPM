3748c186cdb60e51d2da5b33511527b7e1297041
richrosenthal <rrosenthal2013@gmail.com>
2020-11-02 11:38:50 -0500

    change format of what is being rendered

--- frontend/src/task.js
+++ frontend/src/task.js
@@ -1,5 +1,6 @@
 class Task{
-  constructor(description, completion, tools_needs, equipment_id){
+  constructor(id, description, completion, tools_needs, equipment_id){
+    this.id = id;
     this.description = description;
     this.completion = completion;
     this.tools_needs = tools_needs;
@@ -7,32 +8,32 @@ class Task{
   }
 
   renderTask() {
-    let taskDiv = document.getElementById("task-container")
+    let taskDiv = document.getElementById("equipment-container")
 
     taskDiv.innerHTML +=
     `
     <tr>
-      <th>${this.id}</th>
+      <th>
+        <label>Task#</label>
+        ${this.id}
+      </th>
 
       <th>
-
+        <label>Description:</label>
         ${this.description}
       </th>
-      <th>
 
-        ${this.completion}
-      </th>
       <th>
-
+        <label>Tools Needed:</label>
         ${this.tools_needs}
       </th>
-      <th>
 
-        ${this.equipment_id}
-      </th>
     </tr>
-    <button class="delete-button" data-id=${this.id} onclick="deleteEquipment()">Delete EQ</button>
+    <button class="delete-button" data-id=${this.id} onclick="deleteEquipment()">Delete Task</button>
     <br>
+
+
+    
     `
 
 
