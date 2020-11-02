896ef5225d788e0b43d1e7caef51aa45bd29f7c1
richrosenthal <rrosenthal2013@gmail.com>
2020-11-02 17:55:23 -0500

    add extra parameter to constructor

--- frontend/src/equipment.js
+++ frontend/src/equipment.js
@@ -1,18 +1,21 @@
 class Equipment{
-  constructor(id, name, description, location, department){
+  constructor(id, name, description, location, department, tasks){
     this.id = id;
     this.name = name;
     this.description = description;
     this.location = location;
     this.department = department;
+    this.tasks = tasks;
   }
 
   //render instance method
   renderEquipment() {
     let equipmentDiv = document.getElementById("equipment-container")
+    let taskDiv = document.getElementById("task-container")
 
     equipmentDiv.innerHTML +=
     `
+    <h4>
     <tr>
       <th>${this.id}</th>
       <th>
@@ -34,9 +37,19 @@ class Equipment{
     </tr>
     <button class="delete-button" data-id=${this.id} onclick="deleteEquipment()">Delete EQ</button>
     <br>
+    </h4>
+
+
     `
 
 
+      for (const task of this.tasks){
+        let t = new Task(task.id, task.description,
+          task.completion, task.tools_needs, task.equipment_id)
+
+
+        t.renderTask();
+      }
   }
 
 
